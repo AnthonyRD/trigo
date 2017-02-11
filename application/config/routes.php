8963431a65c3841197fd4ee3d8d7c6564e274672
AnthonyRD05 <williamscontreraslugo@gmail.com>
2017-02-11 03:20:38 +0000

    set router and controllers

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,81 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+/**
+ * Account Rauter
+ * */
+$route['login'] = "account/login/index";
+$route['register'] = "account/register/index";
+$route['register/confirm/email'] = "account/confirm/email";
+$route['login/forgot/email'] = "account/forgot/email";
+$route['login/forgot/password'] = "account/forgot/password";
+$route['login/forgot/password/confirm'] = "account/forgot/password_confirm";
+$route['login/reset/password'] = "account/reset/password";
+$route['login/reset/password/confirm'] = "account/reset/password_confirm";
+/**
+ * Customers Rauter
+ * */
+$route['customers/create'] = "customers/create/index";
+$route['customers/delete/(:num)'] = "customers/delete/index/$1";
+$route['customers/delete/confirm'] = "customers/delete/confirm";
+$route['customers/details/(:num)'] = "customers/details/index/$1";
+$route['customers/edit/(:num)'] = "customers/edit/index/$1";
+$route['customers/edit/confirm'] = "customers/edit/confirm";
+$route['customers'] = "customers/index/index";
+/**
+ * Dashboard Raouter
+ * */
+$route['dashboard'] = "dashboard/index/index";
+/**
+ * Products Rauter
+ * */
+$route['products'] = "products/index/index";
+$route['product/create'] = "products/create/index";
+$route['product/create/confirm'] = "products/create/confirm";
+$route['product/delete/(:num)'] = "products/delete/index/$1";
+$route['products/delete/confirm'] = "products/delete/confirm";
+$route['product/edit/(:num)'] = "products/edit/index/$1";
+$route['product/edit/confirm'] = "products/edit/confirm";
+$route['product/details/(:num)'] = "products/details/index/$1";
+/**
+ * Suppliers Router
+ * */
+$route['suppliers'] = "suppliers/index/index";
+$route['supplier/create'] = "suppliers/create/index";
+$route['supplier/create/confirm'] = "suppliers/create/confirm";
+$route['supplier/delete/(:num)'] = "suppliers/delete/index/$1";
+$route['supplier/delete/confirm'] = "suppliers/delete/confirm";
+$route['supplier/edit/(:num)'] = "suppliers/edit/index/$1";
+$route['supplier/edit/confirm'] = "suppliers/edit/confirm";
+$route['suppliers/details/(:num)'] = "suppliers/details/index/$1";
+/**
+ * Orders Rauter
+ * */
+$route['orders'] = "orders/index/index";
+$route['order/create'] = "orders/create/index";
+$route['order/create/confirm'] = "orders/create/confirm";
+$route['order/delete/(:num)'] = "orders/delete/index/$1";
+$route['order/delete/confirm'] = "orders/delete/confirm";
+$route['order/edit/(:num)'] = "orders/edit/index/$1";
+$route['order/edit/confirm'] = "orders/edit/confirm";
+$route['order/details/(:num)'] = "suppliers/details/index/$1";
+/**
+ * Employes Router
+ * */
+$route['employes'] = "employes/index/index";
+$route['employe/create'] = "employes/create/index";
+$route['employe/create/confirm'] = "employes/create/confirm";
+$route['employe/delete/(:num)'] = "employes/delete/index/$1";
+$route['employe/delete/confirm'] = "employes/delete/confirm";
+$route['employe/edit/(:num)'] = "employes/edit/index/$1";
+$route['employe/edit/confirm'] = "employes/edit/confirm";
+$route['employe/details/(:num)'] = "employes/details/index/$1";
+/**
+ * Reports Router
+ * */
+$route['reports'] = "reports/index/index";
